eb6530f9d27f222ff54c81fbed4f5fd05f8be659
Jose Vazquez <jose.vazquez@outcomehealth.com>
2020-09-16 12:59:30 -0500

    nested components

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 class App extends Component {
   render() {
     return (
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Hello World</h5>
-          <h6 class="card-subtitle mb-2 text-muted">First react app</h6>
-          <p class="card-text">Text message</p>
-        </div>
+      <div>
+        <Button></Button>
+        <Display></Display>
       </div>
     );
   }
+}
+
+
+function Button() {
+  const [counter, setCounter] = useState(5);
+  const clickHandler = () => setCounter(counter * 2);
+  return (
+    <button type="button" class="btn btn-primary" onClick={clickHandler}>
+      {counter}
+    </button>
+  )
+}
+
+function Display() {
+  return (
+    <div>...</div>
+  )
 }
 
 export default App;
--- react-api/src/index.js
+++ react-api/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,20 +6,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Button />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-function Button() {
-  const [counter, setCounter] = useState(5);
-  const clickHandler = () => setCounter(counter * 2);
-  return (
-    <button type="button" class="btn btn-primary" onClick={clickHandler}>
-      {counter}
-    </button>
-  )
-}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
