772d162453603c39a7058d1df5074760415d2ce0
Jose Vazquez <jose.vazquez@outcomehealth.com>
2020-09-16 13:26:00 -0500

    passing data among components. moving from class to function components since this way of data flow works only for function components

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -4,28 +4,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Button></Button>
-        <Display></Display>
       </div>
     );
   }
-}
-
-
-function Button() {
-  const [counter, setCounter] = useState(5);
-  const clickHandler = () => setCounter(counter * 2);
-  return (
-    <button type="button" class="btn btn-primary" onClick={clickHandler}>
-      {counter}
-    </button>
-  )
-}
-
-function Display() {
-  return (
-    <div>...</div>
-  )
 }
 
 export default App;
--- react-api/src/index.js
+++ react-api/src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App2 />,
   document.getElementById('root')
 );
+
+function App2() {
+  const [counter, setCounter] = useState(42);
+  const clickHandler = () => setCounter(counter + 1);
+  return (
+    <div>
+      <Button clickHandler={clickHandler} />
+      <Display message={counter} />
+    </div>
+  )
+}
+
+function Button(props) {
+  return (
+    <button type="button" class="btn btn-primary" onClick={props.clickHandler}>
+      +1
+    </button>
+  )
+}
+
+function Display(props) {
+  return (
+    <div>{props.message}</div>
+  )
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
