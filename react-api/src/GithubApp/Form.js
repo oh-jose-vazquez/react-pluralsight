85623a72d045cf37467135ad785f355afcb5d84d
Jose Vazquez <jose.vazquez@outcomehealth.com>
2020-09-17 09:47:00 -0500

    Read input using the state

--- react-api/src/GithubApp/Form.js
+++ react-api/src/GithubApp/Form.js
@@ -2,18 +2,35 @@ import React, { Component } from 'react'
 
 class Form extends Component {
 
-    userNameInput = React.createRef();
+    state = {
+        userName: '',
+        submitted: false,
+    };
 
     handleSubmit = (event) => {
         event.preventDefault(); //<---- Avoid page refreshing when submit the form
-        console.log(this.userNameInput.current.value);
+        console.log(this.state.userName);
+        this.setState({
+            submitted: true
+        });
+        
+    };
+
+    handleOnChange = (event) => {
+        this.setState({
+            userName: event.target.value
+        });
     };
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <div className="input-group mb-3 d-flex justify-content-center">
-                    <input type="text" className="form-control col-4" placeholder="Username" ref={this.userNameInput} required></input>
+                    <input type="text" placeholder="Username"
+                        className={`form-control col-4 ${this.state.submitted && this.state.userName === '' ? 'is-invalid' : ''}`}
+                        value={this.state.userName}
+                        onChange={this.handleOnChange} //Needs this since the value is controled by reatc (no autobinding)
+                    ></input>
                     <div className="input-group-append">
                         <button className="btn btn-outline-secondary" type="submit">Add</button>
                     </div>
