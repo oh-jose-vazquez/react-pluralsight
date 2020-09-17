336635f733a663f88089b3a3fd1b26bf808c8d09
Jose Vazquez <jose.vazquez@outcomehealth.com>
2020-09-17 10:55:26 -0500

    clean code

--- react-api/src/GithubApp/GithubApp.js
+++ react-api/src/GithubApp/GithubApp.js
@@ -2,24 +2,6 @@ import React, { Component } from 'react'
 import Form from './Form.js'
 import CardList from './CardList.js'
 
-// const testData = [
-//     {
-//         name: "name1",
-//         company: "company1",
-//         imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
-//     },
-//     {
-//         name: "name2",
-//         company: "company2",
-//         imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
-//     },
-//     {
-//         name: "name3",
-//         company: "company3",
-//         imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
-//     },
-// ];
-
 class GithubApp extends Component {
 
     state = {
