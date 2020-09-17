2a6141dcd44155300bd424586d6bd3f6979343ca
Jose Vazquez <jose.vazquez@outcomehealth.com>
2020-09-17 10:54:54 -0500

    Call real api

--- react-api/src/GithubApp/Form.js
+++ react-api/src/GithubApp/Form.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+const axios = require('axios');
 
 class Form extends Component {
 
@@ -7,13 +8,30 @@ class Form extends Component {
         submitted: false,
     };
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault(); //<---- Avoid page refreshing when submit the form
         console.log(this.state.userName);
         this.setState({
             submitted: true
         });
-        
+        if (this.state.userName !== '') {
+            try {
+                const response = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+                console.log(response);
+                const profileData = {
+                    name: response.data.name,
+                    company: response.data.company,
+                    imageUrl: response.data.avatar_url
+                }
+                this.props.onSubmit(profileData);
+                this.setState({
+                    userName: ''
+                });
+            } catch(error) {
+                
+            }
+            
+        }
     };
 
     handleOnChange = (event) => {
@@ -27,7 +45,7 @@ class Form extends Component {
             <form onSubmit={this.handleSubmit}>
                 <div className="input-group mb-3 d-flex justify-content-center">
                     <input type="text" placeholder="Username"
-                        className={`form-control col-4 ${this.state.submitted && this.state.userName === '' ? 'is-invalid' : ''}`}
+                        className={`form-control col-md-4 col-sm-8 ${this.state.submitted && this.state.userName === '' ? 'is-invalid' : ''}`}
                         value={this.state.userName}
                         onChange={this.handleOnChange} //Needs this since the value is controled by reatc (no autobinding)
                     ></input>
--- react-api/src/GithubApp/GithubApp.js
+++ react-api/src/GithubApp/GithubApp.js
@@ -2,31 +2,36 @@ import React, { Component } from 'react'
 import Form from './Form.js'
 import CardList from './CardList.js'
 
-const testData = [
-    {
-        name: "name1",
-        company: "company1",
-        imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
-    },
-    {
-        name: "name2",
-        company: "company2",
-        imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
-    },
-    {
-        name: "name3",
-        company: "company3",
-        imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
-    },
-];
+// const testData = [
+//     {
+//         name: "name1",
+//         company: "company1",
+//         imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
+//     },
+//     {
+//         name: "name2",
+//         company: "company2",
+//         imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
+//     },
+//     {
+//         name: "name3",
+//         company: "company3",
+//         imageUrl: "https://avatars1.githubusercontent.com/u/55112875?v=4"
+//     },
+// ];
 
 class GithubApp extends Component {
 
     state = {
-        profiles: testData
+        profiles: []
     };
 
-    addNewProfile = (profileData) => { console.log(profileData) };
+    addNewProfile = (profileData) => { 
+        console.log(profileData);
+        this.setState(prevState => ({
+            profiles: [...prevState.profiles, profileData]
+        }));
+    };
 
     render() {
         return (
